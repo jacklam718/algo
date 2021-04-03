8b415481ac8f67d77875685e2f8a0da4385d9119
jack lam <jacklam718@gmail.com>
2021-04-04 02:12:01 +0800

    increase performance to O(n) for leetcode problem 997

--- leetcode/977-squares-of-a-sorted-array/index.js
+++ leetcode/977-squares-of-a-sorted-array/index.js
@@ -3,7 +3,18 @@
  * @return {number[]}
  */
  var sortedSquares = function(nums) {
-  return nums
-    .map(num => Math.abs(num) * Math.abs(num))
-    .sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length-1;
+  const ans = new Array(nums.length);
+  for (let i = nums.length-1; i >= 0; i--) {
+   if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+      square = nums[right]
+      right--;
+    } else {
+      square = nums[left];
+      left++;
+    }
+    ans[i] = square * square;
+  }
+  return ans;
 };
