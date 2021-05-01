33cd44af20ed077d28869f999e255562d6c35c46
jack lam <jacklam718@gmail.com>
2021-05-02 04:02:30 +0800

    improve performance for leetcode problem 27 remove-element

--- leetcode/27-remove-element/index.js
+++ leetcode/27-remove-element/index.js
@@ -4,10 +4,19 @@
  * @return {number}
  */
  var removeElement = function(nums, val) {
-  for (let i = nums.length; i >= 0; i--) {
-    if (nums[i] === val) {
-      nums.splice(i, 1);
+  let count = 0
+  for (let i = 0, j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      swap(nums, i++, j);
+    } else {
+      count++;
     }
   }
-  return nums.length;
+  nums.length = nums.length - count;
 };
+
+var swap = function(nums, i1, i2) {
+  const temp = nums[i1];
+  nums[i1] = nums[i2];
+  nums[i2] = temp;
+}
