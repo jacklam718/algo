9ec134b013cf52133cc88f00e0541bd6d448d0f7
Jack Lam <jacklam718@gmail.com>
2023-08-14 03:06:10 +0800

    add solution for leedcode 28

---
+++ leetcode/28-find-the-index-of-the-first-occurrence-in-a-string/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack.substring(i, i + needle.length) === needle) {
+      return i;
+    }
+  }
+  return -1;
+};
