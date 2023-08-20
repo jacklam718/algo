1fe8cdadc98a1ef15974173d50ed6ba5ad3168cb
Jack Lam <jacklam718@gmail.com>
2023-08-21 01:33:11 +0800

    simplify version for leetcode problem 14

--- leetcode/14-longest-common-prefix/index.js
+++ leetcode/14-longest-common-prefix/index.js
@@ -3,21 +3,12 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-  let str = strs[0];
-  let prefixLen = 0;
-  if (strs.length === 1) return str;
-  for (let i = 1; i < strs.length; i++) {
-    let s = "";
-    for (let j = 0; j < strs[i].length; j++) {
-      if (str[j] !== strs[i][j]) break;
-      s += str[j];
-    }
-    if (s === "") return s;
-    if (prefixLen > 0) {
-      prefixLen = Math.min(prefixLen, s.length);
-    } else {
-      prefixLen = s.length;
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return strs[0].slice(0, i);
+      }
     }
   }
-  return str.substring(0, prefixLen);
+  return strs[0];
 };
