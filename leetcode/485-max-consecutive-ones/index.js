31512d3efb345e017b9603f13a044f1c71cc0011
Jack Lam <jacklam718@gmail.com>
2023-08-23 02:52:29 +0800

    simplify leetcode solution 485

--- leetcode/485-max-consecutive-ones/index.js
+++ leetcode/485-max-consecutive-ones/index.js
@@ -2,15 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
- var findMaxConsecutiveOnes = function(nums) {
-  let count = maxCount = 0;
-  for (const num of nums) {
-    if (num === 1) {
-      count++;
-    } else {
-      maxCount = Math.max(maxCount, count);
-      count = 0;
-    }
+var findMaxConsecutiveOnes = function (nums) {
+  for (let i = (count = max = 0); i < nums.length; i++) {
+    if (nums[i] === 1) max = Math.max(++count, max);
+    else count = 0;
   }
-  return Math.max(maxCount, count)
+  return max;
 };
