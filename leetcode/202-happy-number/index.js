18cd1d23e04e6553433019c35b53311b4ce07bcd
Jack Lam <jacklam718@gmail.com>
2023-08-31 20:46:01 +0800

    uses while loop instead of recursion for leetcode 202

--- leetcode/202-happy-number/index.js
+++ leetcode/202-happy-number/index.js
@@ -2,13 +2,14 @@
  * @param {number} n
  * @return {boolean}
  */
-var isHappy = function(n, seen = new Set()) {
-  for (var i = sum = 0, digits = digits = n.toString(); i < digits.length; i++) {
-    sum += parseInt(digits.charAt(i)) ** 2;
-  }
-  if (n !== 1 && !seen.has(n)) {
+var isHappy = function(n) {
+  const seen = new Set();
+  while (n !== 1 && !seen.has(n)) {
+    for (var i = sum = 0, digits = n.toString(); i < digits.length; i++) {
+      sum += parseInt(digits.charAt(i)) ** 2;
+    }
     seen.add(n);
-    return isHappy(sum, seen)
+    n = sum;
   }
-  return sum === 1;
+  return n === 1;
 };
