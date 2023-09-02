ff0e0f961004cb9784d7ed4269391767d0d1004e
Jack Lam <jacklam718@gmail.com>
2023-09-03 02:04:09 +0800

    performance tweaking for leetcode 350

--- leetcode/350-intersection-of-two-arrays-ii/index.js
+++ leetcode/350-intersection-of-two-arrays-ii/index.js
@@ -4,24 +4,17 @@
  * @return {number[]}
  */
 var intersect = function (nums1, nums2) {
-  const freq1 = {};
-  const freq2 = {};
-  const n = Math.max(nums1.length, nums2.length);
+  const map = new Map();
   let result = [];
-  for (let i = 0; i < n; i++) {
-    if (nums1[i] !== undefined)
-      freq1[nums1[i]] = freq1[nums1[i]] ? freq1[nums1[i]] + 1 : 1;
-    if (nums2[i] !== undefined)
-      freq2[nums2[i]] = freq2[nums2[i]] ? freq2[nums2[i]] + 1 : 1;
+  for (let i = 0; i < nums1.length; i++) {
+    map.set(nums1[i], (map.get(nums1[i]) || 0) + 1);
   }
 
-  for (let i = 0; i < n; i++) {
-    if (freq2[nums1[i]] !== undefined) {
-      const sum = Math.min(freq1[nums1[i]], freq2[nums1[i]]);
-      // Important requirement, each element in the result must appear as many times as it shows in both arrays.
-      result = result.concat(new Array(sum).fill(null).map(() => nums1[i]));
-      // Remove the element from the frequency map to avoid duplicates.
-      freq2[nums1[i]] = undefined;
+  for (let i = 0; i < nums2.length; i++) {
+    let j = map.get(nums2[i]);
+    if (j > 0) {
+      result.push(nums2[i]);
+      map.set(nums2[i], --j);
     }
   }
   return result;
