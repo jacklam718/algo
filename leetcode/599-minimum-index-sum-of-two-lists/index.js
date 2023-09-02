db33dfcf397ac86970d890ba98f66bb9288ebce4
Jack Lam <jacklam718@gmail.com>
2023-09-02 16:34:47 +0800

    performance tweaking for leetcode 599

--- leetcode/599-minimum-index-sum-of-two-lists/index.js
+++ leetcode/599-minimum-index-sum-of-two-lists/index.js
@@ -7,14 +7,21 @@ var findRestaurant = function (list1, list2) {
   const n = Math.max(list1.length, list2.length);
   let result = [];
   let min = Infinity;
+  const kv1 = {};
+  const kv2 = {};
+
   for (let i = 0; i < n; i++) {
-    const j = list2.findIndex((s) => s === list1[i]);
-    if (j !== -1) {
-      const sum = i + j;
+    if (list1[i]) kv1[list1[i]] = i;
+    if (list2[i]) kv2[list2[i]] = i;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (list1[i] in kv2) {
+      const sum = i + kv2[list1[i]];
       if (sum < min) {
         min = sum;
         result = [list1[i]];
-      } else if (min === sum) {
+      } else if (sum === min) {
         result.push(list1[i]);
       }
     }
